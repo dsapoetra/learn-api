75e37d4b245432a486df1443615358163c561c59
dsapoetra <angga.dimassaputra@gmail.com>
2017-08-05 17:43:05 +0700

    save a data

--- app/src/clj/app/routes/home.clj
+++ app/src/clj/app/routes/home.clj
@@ -2,13 +2,17 @@
   (:require [app.layout :as layout]
             [compojure.core :refer [defroutes GET PUT]]
             [ring.util.http-response :as response]
+            [ring.util.response :as r]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
     "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
-(defn save-transaction [])
+(def data (atom {}))
+
+(defn save-transaction [id body]
+  (swap! data assoc id body))
 
 (defn get-transaction-by-type [])
 
@@ -20,6 +24,14 @@
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+           (GET "/" [] (home-page))
+           (PUT "/transactionservice/transaction/:transaction-id" [transaction-id] (fn [req]
+                                                                                     (let [amount (get-in req [:params :amount])
+                                                                                           types (get-in req [:params :type])
+                                                                                           parent-id (get-in req [:params :parent-id])]
+                                                                                       (save-transaction transaction-id {:amount amount :type types :parent-id parent-id}))))
+           (GET "/transactionservice/transaction/:transaction-id" [] ())
+           (GET "/transactionservice/types/:type" [] ())
+           (GET " /transactionservice/sum/:transaction-id" [] ())
+           (GET "/about" [] (about-page)))
 
